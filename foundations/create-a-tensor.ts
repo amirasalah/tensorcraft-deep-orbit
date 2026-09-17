cbca8764aab47c00f3f08d0b6b7c351148c9cc0c
Amira Salah <amera.salah.89@gmail.com>
2026-09-17 13:25:43 +0200

    feat(foundations): Complete exercise -- create-a-tensor

--- foundations/create-a-tensor.ts
+++ foundations/create-a-tensor.ts
@@ -1,6 +1,6 @@
 const streamValues = [0.12, 0.94, 0.31, 0.88, 0.47];
 
-const reading = tf.tensor(streamValues);
+const reading = tf.tensor(streamValues, [streamValues.length]);
 const shape = reading.shape;
 const values = Array.from(reading.dataSync());
 const total = tf.tidy(() => reading.sum().dataSync()[0]);
